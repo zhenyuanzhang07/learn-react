9f9b4ab1e9e2cad28c6fe996115c04eb3bd1baf2
Zhenyuan Zhang <zhang.zhenyuan@northeastern.edu>
2024-02-10 22:19:28 +0000

    updated src/pages/qcomps/profile_mistake.js and src/pages/qcomps/firstcomp.js

--- src/pages/qcomps/firstcomp.js
+++ src/pages/qcomps/firstcomp.js
@@ -1,1 +1,17 @@
-function MyComp() {}
+import React from 'react'; // Import React
+
+function MyComp() {
+  // Return JSX to render
+  return (
+    <div>
+      <h1>My first component</h1>
+      <ol>
+        <li>Components: UI Building Block</li>
+        <li>Defining a Component</li>
+        <li>Using a component</li>
+      </ol>
+    </div>
+  );
+}
+
+export default MyComp; // Export the component
--- src/pages/qcomps/profile_mistake.js
+++ src/pages/qcomps/profile_mistake.js
@@ -1,4 +1,5 @@
-export default function profile() {
-    return
-        <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />;
+export default function Profile() {
+    return (
+        <img src="https://i.imgur.com/jA8hHMpm.jpg" alt="Katsuko Saruhashi" />
+    );
 }
