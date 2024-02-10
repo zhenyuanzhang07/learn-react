acb13bb8851d0dd1839871d602744be2be6c5c77
Zhenyuan Zhang <zhang.zhenyuan@northeastern.edu>
2024-02-10 22:51:44 +0000

    updated src/pages/components/gallery.js src/pages/components/props_item.js

--- src/pages/components/gallery.js
+++ src/pages/components/gallery.js
@@ -3,10 +3,23 @@ import Profile from './profile.js'
 export default function Gallery() {
   return (
     <div>
-      <h1>Profiles: </h1>
-      <Profile />
-      <Profile />
-      <Profile />
+      <h1>Notable Scientists</h1>
+      <Profile
+        name="Maria Skłodowska-Curie"
+        imageUrl="https://i.imgur.com/szV5sdGs.jpg"
+        altText="Maria Skłodowska-Curie"
+        profession="physicist and chemist"
+        awards="4 (Nobel Prize in Physics, Nobel Prize in Chemistry, Davy Medal, Matteucci Medal)"
+        discoveries="polonium (element)"
+      />
+      <Profile
+        name="Katsuko Saruhashi"
+        imageUrl="https://i.imgur.com/YfeOqp2s.jpg"
+        altText="Katsuko Saruhashi"
+        profession="geochemist"
+        awards="2 (Miyake Prize for geochemistry, Tanaka Prize)"
+        discoveries="a method for measuring carbon dioxide in seawater"
+      />
     </div>
   );
 }
--- src/pages/components/props_item.js
+++ src/pages/components/props_item.js
@@ -1,8 +1,8 @@
 function Item({ name, isPacked }) {
-  if (isPacked) {
-    return <li className="item">{name} ✔</li>;
-  }
-  return <li className="item"> {name} </li>
+  // Use a ternary operator to conditionally render packed items with <del> tag
+  return (
+    <li className="item">{isPacked ? <del>{name}</del> : name}</li>
+  );
 }
 
 export default function PackingList() {
