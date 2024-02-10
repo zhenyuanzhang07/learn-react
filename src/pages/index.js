803bd78c2f5756529c34774101ea2a4699a406fa
Zhenyuan Zhang <zhang.zhenyuan@northeastern.edu>
2024-02-10 22:21:21 +0000

    updated src/pages/index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,18 @@
+import React from 'react';
+import Profile from './qcomps/profile_mistake';
 // import Head from 'next/head'
 // import Image from 'next/image'
 // import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
-import Profile from './components/profile'
+import styles from '@/styles/Home.module.css';
+// Renamed the second Profile import to avoid naming conflict
+import ProfileComponent from './components/profile';
 
 export default function Home() {
   return (
     <div className={styles.main}>
+        {}
         <Profile />
+        {}
     </div>
-  )
+  );
 }
