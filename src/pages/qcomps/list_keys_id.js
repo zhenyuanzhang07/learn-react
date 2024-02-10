d2c5565db1e219ab09dda120c31c8d975e849202
Zhenyuan Zhang <zhang.zhenyuan@northeastern.edu>
2024-02-10 22:55:00 +0000

    updated src/pages/qcomps/list_keys_id.js src/pages/qcomps/recipes.js

--- src/pages/qcomps/list_keys_id.js
+++ src/pages/qcomps/list_keys_id.js
@@ -13,11 +13,13 @@ export const people = [{
 }];
 
 function getImageUrl(imageId) {
-    return "https://i.imgur.com/" + imageId + "s.jpg"
+    return `https://i.imgur.com/${imageId}s.jpg`;
 }
+
 export default function List() {
   const listItems = people.map(person =>
-    <li>
+    <li key={person.id}>
+      <img src={getImageUrl(person.imageId)} alt={person.name} />
     </li>
   );
   return <ul>{listItems}</ul>;
--- src/pages/qcomps/recipes.js
+++ src/pages/qcomps/recipes.js
@@ -16,7 +16,16 @@ export default function RecipeList() {
   return (
     <div>
       <h1>Recipes</h1>
-      {}
+      {recipes.map(recipe => (
+        <div key={recipe.id}>
+          <h2>{recipe.name}</h2>
+          <ul>
+            {[...recipe.ingredients].map((ingredient, index) => (
+              <li key={index}>{ingredient}</li>
+            ))}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 }
